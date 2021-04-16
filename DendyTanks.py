46b496c90e276d77a08688c3e98ebdf7a5ea2e0c
alex <alextmail@yandex.ru>
2021-04-16 17:28:35 +0300

    Added primitive control for player tank
    todo:
     * rotate when change direction.
     * on rotation we should correct position for more easy move near walls
     * maybe not store pressed keys and always handle them as events

--- DendyTanks.py
+++ DendyTanks.py
@@ -17,15 +17,12 @@ class DendyTanks:
     self.screen = pygame.display.set_mode((640, 480))
     self.time = time()
     self.allObjects = []
-    playerTank = GameObject("res/playerTank.png")
-    playerTank.coords = Vector2(320, 240)
-    self.allObjects.append(playerTank)
+    self.allObjects.append(PlayerTank(Vector2(320, 240)))
+    self.control = Control()
 
   def mainloop(self):
     while 1:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit()
+      self.control.handleEvents()
       self.update()
       self.render()
 
@@ -34,7 +31,7 @@ class DendyTanks:
     self.time = time()
     dt = self.time - prevTime
     for obj in self.allObjects:
-      obj.update(dt)
+      obj.update(dt, self.control)
 
   def render(self):
     backGroundColor = (20, 20, 100)
@@ -52,12 +49,47 @@ class GameObject():
     self.image = pygame.image.load(resourceName) if resourceName else None
     self.rect = self.image.get_rect()
 
-  def update(self, dt):
+  def update(self, dt, control=None):
     self.coords += dt * self.speed
     self.rect.update(self.coords, self.rect.size)
 
   def render(self, screen):
     screen.blit(self.image, self.rect)
+
+
+class PlayerTank(GameObject):
+  def __init__(self, pos):
+    super().__init__("res/playerTank.png")
+    self.coords = pos
+
+  def update(self, dt, control):
+    direction = Vector2(0, 0)
+    if pygame.K_UP in control.pressedKeys:
+      direction = Vector2(0, -1)
+    elif pygame.K_DOWN in control.pressedKeys:
+      direction = Vector2(0, 1)
+    elif pygame.K_LEFT in control.pressedKeys:
+      direction = Vector2(-1, 0)
+    elif pygame.K_RIGHT in control.pressedKeys:
+      direction = Vector2(1, 0)
+    self.speed = direction * 120
+    super().update(dt, control)
+
+
+class Control():
+  """Contain input from user"""
+  def __init__(self):
+    self.pressedKeys = set()
+
+  def handleEvents(self):
+    for event in pygame.event.get():
+      # print(event)
+      if event.type == pygame.QUIT:
+        sys.exit()
+      elif event.type == pygame.KEYDOWN:
+        self.pressedKeys.add(event.key)
+      elif event.type == pygame.KEYUP:
+        self.pressedKeys.discard(event.key)
 
 
 def main():
